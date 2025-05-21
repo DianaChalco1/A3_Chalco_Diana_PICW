eabb74ac41f149127c6609f09971ff67cf9c62bc
DianaChalco1 <dcchalco@espe.edu.ec>
2025-05-21 00:23:04 -0500

    actualizaciòn del codigo edadCategoria

--- public/js/edadCategoria.js
+++ public/js/edadCategoria.js
@@ -1,26 +1,20 @@
-let edad = prompt("Ingrese su edad:");
+let edad = prompt("¿Cuántos años tienes?");
 
 // Convertimos la entrada a número
 edad = Number(edad);
 
+// Verificamos si es un número válido
 if (isNaN(edad)) {
     console.log("Por favor, ingrese un número válido.");
+} else if (edad < 0) {
+    console.log("Edad no válida");
+} else if (edad > 0 && edad <= 12) {
+    console.log("Niño");
+} else if (edad > 13 && edad <= 17) {
+    console.log("Adolescente");
+} else if (edad <= 59) {
+    console.log("Adulto");
 } else {
-    if (edad < 0) {
-        console.log("Edad no válida");
-    } else {
-        if (edad <= 12) {
-            console.log("Niño");
-        } else {
-            if (edad <= 17) {
-                console.log("Adolescente");
-            } else {
-                if (edad <= 59) {
-                    console.log("Adulto");
-                } else {
-                    console.log("Adulto mayor");
-                }
-            }
-        }
-    }
+    console.log("Adulto mayor");
 }
+
